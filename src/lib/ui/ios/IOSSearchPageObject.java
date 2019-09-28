d57eb7dd90e69d52caebf90f2afe4d115df127b0
lapshova-m <lapshova.marina.smr@gmail.com>
2019-09-28 15:02:33 +0400

    Update test for searching three articles - iOS test

--- src/lib/ui/ios/IOSSearchPageObject.java
+++ src/lib/ui/ios/IOSSearchPageObject.java
@@ -9,7 +9,7 @@ public class IOSSearchPageObject extends SearchPageObject {
         SEARCH_INPUT = "xpath://XCUIElementTypeSearchField[@value='Search Wikipedia']";
         SEARCH_CANCEL_BUTTON = "id:Close";
         SEARCH_RESULT_BY_SUBSTRING_TPL = "xpath://XCUIElementTypeLink[contains(@name, '{SUBSTRING}')]";
-        SEARCH_RESULT_BY_TITLE_AND_DESCRIPTION_TPL = "xpath://XCUIElementTypeLink[contains(name, '{SUBSTRING_TITLE} {SUBSTRING_DESCRIPTION}')]";
+        SEARCH_RESULT_BY_TITLE_AND_DESCRIPTION_TPL = "xpath://XCUIElementTypeLink[(contains(@name, '{SUBSTRING_TITLE}')) and (contains(@name, '{SUBSTRING_DESCRIPTION}'))]";
         SEARCH_RESULT_ELEMENT = "xpath://XCUIElementTypeLink";
         SEARCH_EMPTY_RESULT_ELEMENT = "xpath://XCUIElementTypeStaticText[@name='No results found']";
         LIST_ITEMS_IN_RESULT = "xpath://XCUIElementTypeLink";
